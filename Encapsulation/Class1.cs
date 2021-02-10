4c31d9561ed2f008674b6649fb75bd341c9f8be8
LalithaNadimpalli <lalli.nadimpalli@gmail.com>
2021-02-10 13:16:26 -0800

    Assignment-0(Basics of C#)
    Assignment-0(Basics of C#)

---
+++ Encapsulation/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+
+    class Person
+    {
+        public string Name    // property
+        { get; set; }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Polymorphism
+{
+   
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Animal myAnimal = new Animal();  // Create a Animal object
+                Animal myPig = new Pig();  // Create a Pig object
+                Animal myDog = new Dog();  // Create a Dog object
+
+                myAnimal.animalSound();
+                myPig.animalSound();
+                myDog.animalSound();
+            }
+        }
+    }
